1032c895e3372d40d93e0fbd6a16ee2f75c027f2
Hansraj8149 <hansrajsaini8149@gmail.com>
2023-11-12 17:29:48 +0530

    Hansraj - made some changes in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,14 @@ import { RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server'
 import { LoginLink } from '@kinde-oss/kinde-auth-nextjs/server'
 import Link from 'next/link'
 import React from 'react'
-// import MaxWidthWrapper from "./MaxWidthWrapper";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { buttonVariants } from './ui/button';
+import { ArrowRight } from 'lucide-react';
 
 const Navbar = () => {
   return (
     <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
-        {/* <MaxWidthWrapper> */}
+        <MaxWidthWrapper>
         <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
            <Link href='/' className='flex z-40 font-semibold'>
             <span>chitchatpdf.</span>
@@ -16,14 +18,30 @@ const Navbar = () => {
            <div className="hidden items-center space-x-4 sm:flex">
             <>
             {/* add the button variants class to all the links */}
-            <Link href='/pricing' >Pricing </Link>
-            <LoginLink> Sign in</LoginLink>
-            <RegisterLink>Get Started</RegisterLink>
+            <Link href='/pricing' className={
+              buttonVariants({
+                variant:'ghost',
+                size:'sm',
+
+              })
+                
+            } >Pricing </Link>
+            <LoginLink className={
+              buttonVariants({
+                variant:'ghost',
+                size:'sm',
+              })
+            }> Sign in</LoginLink>
+            <RegisterLink className={
+              buttonVariants({
+                size:'sm',
+              })
+            }>Get Started <ArrowRight className='ml-1.5 h-5'/></RegisterLink>
             </>
            </div>
         </div>
 
-        {/* </MaxWidthWrapper> */}
+        </MaxWidthWrapper>
 
     </nav>
   )
